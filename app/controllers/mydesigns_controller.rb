038282b8cb68879db10372af812a631ce75d3fab
abchi <5568900+abchi@users.noreply.github.com>
2020-06-29 17:55:24 +0900

    feat: メインページの作成

--- app/controllers/mydesigns_controller.rb
+++ app/controllers/mydesigns_controller.rb
@@ -1,4 +1,5 @@
 class MydesignsController < ApplicationController
   def index
+    @tweets = Tweet.where.not(tweet_created_at: "1970-01-01 00:00:00").order(tweet_created_at: "DESC").page(params[:page]).per(9)
   end
 end
